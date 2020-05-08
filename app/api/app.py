596cfd13bc29a8876d00e11319015a1799319495
Kenji Marshall <kenji.marshall99@gmail.com>
2020-05-08 17:33:25 -0400

    created react app and flask API templates

---
+++ app/api/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from flask_bootstrap import Bootstrap
+
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config["MONGO_URI"] = "mongodb+srv://kenji:alexachung#1@hexualdb-yhauo.mongodb.net/test?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+db = mongo.cx.data  # get to db via MongoClient
+
+
+@app.route("/")
+def home_page():
+    cursor = db.albums.find_one({})
+    # otherwise of type ObjectID which isn't JSON serializable
+    cursor['_id'] = str(cursor["_id"])
+    print(cursor)
+    return cursor
